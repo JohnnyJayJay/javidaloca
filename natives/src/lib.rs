2e139662b10edda76acea873d7d55391b7d13879
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-04-08 20:42:40 +0200

    Fix some of the errors

--- natives/src/lib.rs
+++ natives/src/lib.rs
@@ -1,30 +1,34 @@
 use jni::JNIEnv;
 use jni::objects::{JObject, JValue};
 use std::sync::MutexGuard;
+use std::any::Any;
 
 #[no_mangle]
 pub extern "system" fn Java_io_github_javidaloca_RustObject_00024RustPointer_surrender(
     env: JNIEnv,
     object: JObject,
 ) {
+    // FIXME annotate type
     let _pointer = env.take_rust_field(object, "pointer")
         .expect("Could not find pointer field");
 }
 
-fn surrender_rust_pointer<T>(env: JNIEnv, object: JObject, value: T) {
+// FIXME Send requirement
+fn surrender_rust_pointer<'a, T>(env: &'a JNIEnv, object: JObject<'a>, value: T) {
     let pointer_obj = get_pointer_obj(env, object);
     env.set_rust_field(pointer_obj, "pointer", value);
 }
 
-// FIXME lifetimes
-fn get_rust_pointer<T>(env: JNIEnv, object: JObject) -> MutexGuard<T> {
+// FIXME Send requirement
+fn get_rust_pointer<'a, T>(env: &'a JNIEnv, object: JObject<'a>) -> MutexGuard<'a, T> {
     let pointer_obj = get_pointer_obj(env, object);
     env.get_rust_field(pointer_obj, "pointer")
         .expect("Could not find pointer in pointer object")
 }
 
-fn get_pointer_obj<'a>(env: JNIEnv, object: JObject) -> JValue<'a> {
+fn get_pointer_obj<'a>(env: &'a JNIEnv, object: JObject<'a>) -> JObject<'a> {
     env.get_field(
         object, "pointer", "Lio/github/javidaloca/RustObject00024RustPointer"
-    ).expect("Supposed rust object had no pointer object field")
+    ).expect("Supposed rust object had no pointer field")
+        .l().expect("Pointer field was no object")
 }
