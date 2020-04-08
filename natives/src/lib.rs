352e3df34d2bbcf2cb6369eb61f32cf0c063911b
JohnnyJayJay <johnnyjayjay02@gmail.com>
2020-04-09 00:28:45 +0200

    Fix compile errors in rust code

--- natives/src/lib.rs
+++ natives/src/lib.rs
@@ -1,26 +1,27 @@
 use jni::JNIEnv;
-use jni::objects::{JObject, JValue};
+use jni::objects::JObject;
 use std::sync::MutexGuard;
-use std::any::Any;
 
+// TODO check if this works with type parameters (at runtime)
 #[no_mangle]
-pub extern "system" fn Java_io_github_javidaloca_RustObject_00024RustPointer_surrender(
+pub extern "system" fn Java_io_github_javidaloca_RustObject_00024RustPointer_surrender<T>(
     env: JNIEnv,
     object: JObject,
-) {
-    // FIXME annotate type
-    let _pointer = env.take_rust_field(object, "pointer")
-        .expect("Could not find pointer field");
+) where T : 'static + Send {
+    let _value: T = env.take_rust_field(object, "pointer")
+        .expect("Could not find pointer in pointer object");
 }
 
-// FIXME Send requirement
-fn surrender_rust_pointer<'a, T>(env: &'a JNIEnv, object: JObject<'a>, value: T) {
+fn surrender_rust_pointer<'a, T>(env: &'a JNIEnv, object: JObject<'a>, value: T)
+where T : 'static + Send {
     let pointer_obj = get_pointer_obj(env, object);
-    env.set_rust_field(pointer_obj, "pointer", value);
+    env.set_rust_field(pointer_obj, "pointer", value)
+        .expect("Could not find pointer in pointer object");
 }
 
-// FIXME Send requirement
-fn get_rust_pointer<'a, T>(env: &'a JNIEnv, object: JObject<'a>) -> MutexGuard<'a, T> {
+fn get_rust_pointer<'a, T>(env: &'a JNIEnv, object: JObject<'a>) -> MutexGuard<'a, T>
+where T : 'static + Send
+{
     let pointer_obj = get_pointer_obj(env, object);
     env.get_rust_field(pointer_obj, "pointer")
         .expect("Could not find pointer in pointer object")
